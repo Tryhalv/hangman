df94c15452cc09061baf32754870d215d57d2857
Trym Halvorsen <1trymhalvorsen@gmail.com>
2024-12-09 14:37:05 +0100

    added missing files

---
+++ dictionary.mjs
@@ -0,0 +1,28 @@
+const dictionary = {
+  no: {
+    wrongGuesses: "Feil gjetninger",
+    guessPrompt: "Gjett en bokstav eller ord ",
+    winCelibration: "Hurra du gjettet ordet",
+    startingQuestion: "klar til å spille? j/n",
+    languageChoice: "Hvilket språk vil du velge?",
+    languageCode: "NO",
+    startPrompt: "Trykk på en knapp for å starte",
+    wordChoices: "kiwi, bil, hund, etwas",
+    deathScene: "Du har daua",
+    youAreWrong: "du tar feil!!!!",
+  },
+
+  en: {
+    wrongGuesses: "Wrong guess",
+    guessPrompt: "Guess a letter or word",
+    winCelibration: "You did it, you guessed the word",
+    startingQuestion: "Ready to play y/n?",
+    languageChoice: "What language do you want to choose?",
+    languageCode: "EN",
+    startPrompt: "Press any key to start",
+    wordChoices: "kiwi, car, dog, etwas",
+    deathScene: "You died",
+  },
+};
+
+export default dictionary;
